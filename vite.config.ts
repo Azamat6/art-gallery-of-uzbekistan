22f52e57b2eb1456d795435647a792efcfcaa321
Azamat6 <scyazamine@gmail.com>
2024-12-18 11:04:05 +0500

    no plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 
 
@@ -9,14 +9,14 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 export default defineConfig({
   plugins: [
     react(),
-    ViteImageOptimizer({
-      png: {
-        quality: 40,
-      },
-      jpg: {
-        quality: 30,
-      },
-    }),
+    // ViteImageOptimizer({
+    //   png: {
+    //     quality: 40,
+    //   },
+    //   jpg: {
+    //     quality: 30,
+    //   },
+    // }),
   ],
   assetsInclude: ['src/assets/**/*.JPG'],
   build: {
