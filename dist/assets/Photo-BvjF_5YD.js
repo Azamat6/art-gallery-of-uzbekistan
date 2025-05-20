8c5dce4c84fda37ebfb87bf2002031e6917739d1
Azamat6 <scyazamine@gmail.com>
2025-05-21 00:55:30 +0500

    motion1

---
+++ dist/assets/Photo-BvjF_5YD.js
@@ -0,0 +1,1 @@
+import{j as a}from"./lenis-react-Cmd4k6GF.js";const t=()=>a.jsx("div",{className:"Parallax"});export{t as default};
--- dist/assets/Photo-DIO6kQBV.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./lenis-react-CEdfZcFb.js";const t=()=>a.jsx("div",{className:"Parallax"});export{t as default};
