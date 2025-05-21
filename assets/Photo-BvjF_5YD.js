09b91fbcc880b4aa4138b06feb16160e1206c064
Azamat6 <scyazamine@gmail.com>
2025-05-21 21:00:05 +0500

    bg ios issue fix

--- assets/Photo-BvjF_5YD.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./lenis-react-Cmd4k6GF.js";const t=()=>a.jsx("div",{className:"Parallax"});export{t as default};
---
+++ assets/Photo-DVtPlMCs.js
@@ -0,0 +1,1 @@
+import{j as a}from"./lenis-react-Ca9cvzvh.js";const s=()=>a.jsx("div",{className:"parallaxWrapper",children:a.jsx("div",{className:"Parallax"})});export{s as default};
