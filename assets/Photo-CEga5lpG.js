2d9d76fc5dbfaac9d7fac186ace3d98c7eae9a73
Azamat Sultanov <azasultanov06@gmail.com>
2026-02-09 23:00:07 -0600

    collection modify

---
+++ assets/Photo-CEga5lpG.js
@@ -0,0 +1,1 @@
+import{j as a}from"./bootstrap-B7ROO5XB.js";const s=()=>a.jsx("div",{className:"parallaxWrapper",children:a.jsx("div",{className:"Parallax"})});export{s as default};
---
+++ assets/Title-Dqx2aa-v.js
@@ -0,0 +1,1 @@
+const a="/art-gallery-of-uzbekistan/assets/Title-D210bQHC.jpg";export{a as B};
